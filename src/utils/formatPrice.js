7d653b8c8b4029fa45db5072622b8fa143166586
hangtime319 <hangtime31@hotmail.com>
2026-08-12 17:11:56 -0300

    chore(gh-pages): publish site

--- src/utils/formatPrice.js
+++
@@ -1,9 +0,0 @@
-export const formatPrice = (item, billingCycle) => {
-
-    if (billingCycle !== "monthly" && billingCycle !== "yearly") {
-        throw new Error("Billing Cycle not found!");        
-    }
-    
-     return billingCycle === "monthly" ? `$${item.priceMonthly}/mo` : `$${item.priceYearly}/yr`;
-
-}
